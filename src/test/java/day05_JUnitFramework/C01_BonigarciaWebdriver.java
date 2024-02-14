d28387ae6e1d7d8e20edf0238606a33aaa33acad
Mervan Kılıç <mervanalikilic@gmail.com>
2024-02-14 15:47:54 +0300

    5.gun sonu

---
+++ src/test/java/day05_JUnitFramework/C01_BonigarciaWebdriver.java
@@ -0,0 +1,31 @@
+package day05_JUnitFramework;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C01_BonigarciaWebdriver {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // Bir class'ta test işlemlerine başlamadan önce
+        // ilk olarak Webdriver objemizi olusturmali
+        // ve gerekli ayarlamalari yapmaliyiz
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://testotomasyonu.com");
+
+        Thread.sleep(2000);
+        driver.quit();
+
+
+
+
+    }
+}
